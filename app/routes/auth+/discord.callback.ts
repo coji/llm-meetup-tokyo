1a14f28d405a2666beb84ea7e65d163f26a2dfce
coji <coji@techtalk.jp>
2023-06-30 16:31:15 +0900

    feat: redirect to on login

--- app/routes/auth+/discord.callback.ts
+++ app/routes/auth+/discord.callback.ts
@@ -1,14 +1,21 @@
 import type { LoaderArgs } from '@remix-run/node'
 import { authenticator } from '~/services/auth.server'
+import { returnToCookie } from '~/services/session.server'
 import { buildForwardedRequest } from '~/utils/forwarded-request.server'
 
 export const loader = async ({ request }: LoaderArgs) => {
+  // get the returnTo from the cookie
+  const returnTo =
+    ((await returnToCookie.parse(request.headers.get('Cookie'))) as
+      | string
+      | null) ?? '/'
+
   return await authenticator.authenticate(
     'discord',
     buildForwardedRequest(request),
     {
-      successRedirect: '/',
-      failureRedirect: '/',
+      successRedirect: returnTo,
+      failureRedirect: '/login',
     },
   )
 }
