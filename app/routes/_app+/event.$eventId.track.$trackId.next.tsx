de6679e62699705b3e4538273036325d7a79e6e3
coji <coji@techtalk.jp>
2023-07-04 20:40:36 +0900

    fix: auto scroll

--- app/routes/_app+/event.$eventId.track.$trackId.next.tsx
+++ app/routes/_app+/event.$eventId.track.$trackId.next.tsx
@@ -73,101 +73,95 @@ export default function TrackNextPresenterPage() {
   return (
     <Modal isOpen={true} onClose={handleOnClose}>
       <ModalOverlay />
-      <ModalContent>
+      <ModalContent maxH="80dvh">
         <ModalHeader>
           {demoTrack.currentPresenter
             ? 'Set Next Presenter'
             : 'Set First Presenter'}
         </ModalHeader>
         <ModalCloseButton />
-        <ModalBody>
-          <Stack>
-            <Form id="next-presenter-form" method="POST">
-              <Stack>
-                <HStack align="center" h="64px">
-                  <Text fontSize="sm" color="gray.500" fontWeight="bold">
-                    Selected
-                  </Text>
-                  {selectedGuest ? (
-                    <>
-                      <input
-                        type="hidden"
-                        name="presenterId"
-                        defaultValue={selectedGuest?.id}
-                      />
-                      <Avatar
-                        size="md"
-                        src={selectedGuest.lumaUser.avatarUrl}
-                      />
-                      <Box>
-                        <Text>
-                          {selectedGuest.lumaUser.name ?? 'Anonymous'}
-                        </Text>
-                        <Text color="gray.500" fontSize="xs">
-                          {selectedGuest.answers.sns}
-                        </Text>
-                      </Box>
-                    </>
-                  ) : (
-                    <Box fontSize="sm" color="gray.500">
-                      未選択
+        <ModalBody overflow="auto">
+          <Form id="next-presenter-form" method="POST">
+            <Stack>
+              <HStack align="center" h="64px">
+                <Text fontSize="sm" color="gray.500" fontWeight="bold">
+                  Selected
+                </Text>
+                {selectedGuest ? (
+                  <>
+                    <input
+                      type="hidden"
+                      name="presenterId"
+                      defaultValue={selectedGuest?.id}
+                    />
+                    <Avatar size="md" src={selectedGuest.lumaUser.avatarUrl} />
+                    <Box>
+                      <Text>{selectedGuest.lumaUser.name ?? 'Anonymous'}</Text>
+                      <Text color="gray.500" fontSize="xs">
+                        {selectedGuest.answers.sns}
+                      </Text>
                     </Box>
-                  )}
+                  </>
+                ) : (
+                  <Box fontSize="sm" color="gray.500">
+                    未選択
+                  </Box>
+                )}
+              </HStack>
+
+              <FormControl display="flex" alignItems="center">
+                <FormLabel mb="0">Discordに通知</FormLabel>
+                <Switch name="isPushDiscord" defaultChecked value="true" />
+              </FormControl>
+            </Stack>
+          </Form>
+
+          <Form
+            onChange={(e) => {
+              submit(e.currentTarget, { method: 'GET' })
+            }}
+          >
+            <Input
+              type="search"
+              name="search"
+              placeholder="ゲスト名の一部を入力して検索"
+              defaultValue={search}
+            />
+          </Form>
+
+          <Box fontSize="sm" color="gray.600" fontWeight="bold">
+            {search && <span>"{search}"の検索結果:</span>} {guests.length}人
+            アルファベット順
+          </Box>
+
+          <Stack flex="1" spacing="1">
+            {guests.map((guest) => {
+              return (
+                <HStack
+                  key={guest.id}
+                  py="1"
+                  px="2"
+                  rounded="md"
+                  bg={selectedGuest?.id === guest.id ? 'blue.200' : undefined}
+                  _hover={{ bg: 'gray.200', cursor: 'pointer' }}
+                  onClick={() => {
+                    if (selectedGuest?.id === guest.id) {
+                      setSelectedGuest(undefined)
+                    } else {
+                      setSelectedGuest(guest)
+                    }
+                  }}
+                >
+                  <Avatar size="sm" src={guest.lumaUser.avatarUrl}></Avatar>
+                  <Box>
+                    <Text>{guest.lumaUser.name ?? 'Anonymous'}</Text>
+                    <Text color="gray.500" fontSize="xs">
+                      {guest.answers.sns}
+                    </Text>
+                  </Box>
                 </HStack>
-                <FormControl>
-                  <FormLabel>Discordに通知</FormLabel>
-                  <Switch name="isPushDiscord" defaultChecked value="true" />
-                </FormControl>
-              </Stack>
-            </Form>
-
-            <Form
-              onChange={(e) => {
-                submit(e.currentTarget, { method: 'GET' })
-              }}
-            >
-              <Input
-                type="search"
-                name="search"
-                placeholder="ゲスト名の一部を入力して検索"
-                defaultValue={search}
-              />
-            </Form>
-
-            <Box fontSize="sm" color="gray.600" fontWeight="bold">
-              {search && <span>"{search}"の検索結果:</span>} {guests.length}人
-              アルファベット順
-            </Box>
-
-            <Box maxH="60dvh" overflow="auto">
-              <Stack spacing="1">
-                {guests.map((guest) => {
-                  return (
-                    <HStack
-                      key={guest.id}
-                      py="1"
-                      px="2"
-                      rounded="md"
-                      bg={
-                        selectedGuest?.id === guest.id ? 'blue.200' : undefined
-                      }
-                      _hover={{ bg: 'gray.200', cursor: 'pointer' }}
-                      onClick={() => {
-                        setSelectedGuest(guest)
-                      }}
-                    >
-                      <Avatar size="sm" src={guest.lumaUser.avatarUrl}></Avatar>
-                      <Box>
-                        <Text>{guest.lumaUser.name ?? 'Anonymous'}</Text>
-                        <Text color="gray.500" fontSize="xs">
-                          {guest.answers.sns}
-                        </Text>
-                      </Box>
-                    </HStack>
-                  )
-                })}
-              </Stack>
-            </Box>
+              )
+            })}
           </Stack>
         </ModalBody>
 
