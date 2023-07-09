10ca5376bfc177dd8eff9a22483c3b304142b326
coji <coji@techtalk.jp>
2023-07-09 21:26:13 +0900

    feat: server side cache event

--- app/models/luma-event.server.ts
+++ app/models/luma-event.server.ts
@@ -1,37 +1,57 @@
 import type { LumaEvent, Prisma } from '@prisma/client'
+import { cachified } from 'cachified'
 import { prisma } from '~/services/database.server'
+import { lru } from '~/services/lru-cache.server'
 
 export const listLumaEvents = async () => {
-  return await prisma.lumaEvent.findMany({
-    include: {
-      lumaEventGuest: {
-        include: { lumaUser: true },
-        where: { approvalStatus: { in: ['approved', 'invited'] } },
-        orderBy: { createdAt: 'desc' },
-      },
+  return await cachified({
+    key: 'events',
+    cache: lru,
+    getFreshValue: async () => {
+      return await prisma.lumaEvent.findMany({
+        include: {
+          lumaEventGuest: {
+            include: { lumaUser: true },
+            where: { approvalStatus: { in: ['approved', 'invited'] } },
+            orderBy: { createdAt: 'desc' },
+          },
+        },
+        orderBy: { startAt: 'desc' },
+      })
     },
-    orderBy: { startAt: 'desc' },
   })
 }
 
 export const upsertLumaEvent = async (data: Prisma.LumaEventCreateInput) => {
-  return await prisma.lumaEvent.upsert({
+  const ret = await prisma.lumaEvent.upsert({
     where: { id: data.id },
     create: data,
     update: data,
   })
+  lru.delete('events')
+  lru.delete(`event-${data.id}`)
+  return ret
 }
 
 export const updateLumaEvent = async (
   id: LumaEvent['id'],
   data: Prisma.LumaEventUpdateInput,
 ) => {
-  return await prisma.lumaEvent.update({
+  const ret = await prisma.lumaEvent.update({
     where: { id },
     data,
   })
+  lru.delete('events')
+  lru.delete(`event-${id}`)
+  return ret
 }
 
 export const getEventById = async (id: string) => {
-  return await prisma.lumaEvent.findFirstOrThrow({ where: { id } })
+  return await cachified({
+    key: `event-${id}`,
+    cache: lru,
+    getFreshValue: async () => {
+      return await prisma.lumaEvent.findFirstOrThrow({ where: { id } })
+    },
+  })
 }
