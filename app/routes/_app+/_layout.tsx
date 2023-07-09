bb6fb36eaa0f777d9d7133f36bbc191916d3e6bf
coji <coji@techtalk.jp>
2023-07-09 16:21:04 +0900

    feat: page transition progress

--- app/routes/_app+/_layout.tsx
+++ app/routes/_app+/_layout.tsx
@@ -1,6 +1,6 @@
-import { Container, Stack } from '@chakra-ui/react'
+import { Container, Progress, Stack } from '@chakra-ui/react'
 import { json, type LoaderArgs } from '@remix-run/node'
-import { Outlet } from '@remix-run/react'
+import { Outlet, useNavigation } from '@remix-run/react'
 import { AppBreadcrumbs, AppFooter, AppHeader } from '~/components'
 import { useAppBreadcrumbs } from '~/hooks/use-app-breadcrumbs'
 import { requireUser } from '~/services/auth.server'
@@ -18,21 +18,32 @@ export const loader = async ({ request }: LoaderArgs) => {
 
 export default function AppLayout() {
   const breadcrumbs = useAppBreadcrumbs()
+  const navigation = useNavigation()
+
   return (
-    <>
-      <Container
-        maxW="container.lg"
-        display="grid"
-        gridTemplateRows="auto 1fr auto"
-        minH="100dvh"
-      >
-        <AppHeader title="LLM Meetup Tokyo" to="/" />
-        <Stack>
-          <AppBreadcrumbs items={breadcrumbs} />
-          <Outlet />
-        </Stack>
-        <AppFooter />
-      </Container>
-    </>
+    <Container
+      maxW="container.lg"
+      display="grid"
+      gridTemplateRows="auto 1fr auto"
+      minH="100dvh"
+    >
+      {navigation.state !== 'idle' && (
+        <Progress
+          size="xs"
+          colorScheme="discord"
+          isIndeterminate
+          position="fixed"
+          top="0"
+          left="0"
+          right="0"
+        />
+      )}
+      <AppHeader title="LLM Meetup Tokyo" to="/" />
+      <Stack>
+        <AppBreadcrumbs items={breadcrumbs} />
+        <Outlet />
+      </Stack>
+      <AppFooter />
+    </Container>
   )
 }
