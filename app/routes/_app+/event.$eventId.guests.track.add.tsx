58c8fd0c44a0edfd20dfa73b6031b186a5523301
coji <coji@techtalk.jp>
2023-08-10 10:59:06 +0900

    refactor: shadcn/ui

--- app/routes/_app+/event.$eventId.guests.track.add.tsx
+++ app/routes/_app+/event.$eventId.guests.track.add.tsx
@@ -1,31 +1,33 @@
-import {
-  Button,
-  FormControl,
-  FormErrorMessage,
-  FormLabel,
-  HStack,
-  Input,
-  Modal,
-  ModalBody,
-  ModalCloseButton,
-  ModalContent,
-  ModalFooter,
-  ModalHeader,
-  ModalOverlay,
-  Radio,
-  RadioGroup,
-  Select,
-  Spacer,
-  Stack,
-  Text,
-} from '@chakra-ui/react'
-import { useForm } from '@conform-to/react'
+import { conform, useForm } from '@conform-to/react'
 import { parse } from '@conform-to/zod'
 import type { ActionArgs, LoaderArgs } from '@remix-run/node'
-import { Form, useNavigate } from '@remix-run/react'
-import { redirect, typedjson, useTypedLoaderData } from 'remix-typedjson'
+import { json, redirect } from '@remix-run/node'
+import { Form, useLoaderData, useNavigate } from '@remix-run/react'
 import { z } from 'zod'
 import { zx } from 'zodix'
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+  Button,
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  HStack,
+  Input,
+  Label,
+  RadioGroup,
+  RadioGroupItem,
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+  Spacer,
+  Stack,
+} from '~/components/ui'
 import { createDemoTrack, getEventById, listEventGuests } from '~/models'
 import { demoTrackSchema } from '~/schemas/model'
 import { emitter } from '~/services/emitter.server'
@@ -36,7 +38,7 @@ export const loader = async ({ params }: LoaderArgs) => {
   })
 
   const guests = await listEventGuests(eventId)
-  return typedjson({ eventId, guests })
+  return json({ eventId, guests })
 }
 
 export const action = async ({ params, request }: ActionArgs) => {
@@ -53,7 +55,7 @@ export const action = async ({ params, request }: ActionArgs) => {
 }
 
 export default function TrackAddPage() {
-  const { guests } = useTypedLoaderData<typeof loader>()
+  const { guests } = useLoaderData<typeof loader>()
   const [form, { title, hostId, zoomUrl, state }] = useForm({
     defaultValue: { state: 'In Preparation' },
     onValidate({ formData }) {
@@ -66,70 +68,90 @@ export default function TrackAddPage() {
   }
 
   return (
-    <Modal isOpen={true} onClose={handleOnClose}>
-      <ModalOverlay />
-      <ModalContent>
-        <ModalHeader>Add a demo track</ModalHeader>
-        <ModalCloseButton />
-        <ModalBody>
-          <Form id="track-edit-form" method="POST" {...form.props}>
-            <Stack>
-              <FormControl isInvalid={!!title.error}>
-                <FormLabel>Title</FormLabel>
-                <Input
-                  type="text"
-                  name={title.name}
-                  defaultValue={title.defaultValue}
-                  autoFocus
-                />
-                <FormErrorMessage>{title.error}</FormErrorMessage>
-              </FormControl>
+    <Dialog open onOpenChange={handleOnClose}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Add a demo track</DialogTitle>
+        </DialogHeader>
+        <Form id="track-edit-form" method="POST" {...form.props}>
+          <Stack>
+            <fieldset>
+              <Label id={title.id}>Title</Label>
+              <Input {...conform.input(title)} autoFocus />
+              <div className="text-destructive">{title.error}</div>
+            </fieldset>
 
-              <FormControl isInvalid={!!hostId.error}>
-                <FormLabel>Host</FormLabel>
-                <Select name={hostId.name} defaultValue={hostId.defaultValue}>
-                  <option></option>
+            <fieldset>
+              <Label>Host</Label>
+              <Select>
+                <SelectTrigger>
+                  <SelectValue {...conform.select(hostId)} />
+                </SelectTrigger>
+                <SelectContent className="max-h-48 overflow-auto">
+                  <SelectItem value=""></SelectItem>
                   {guests.map((guest) => (
-                    <option key={guest.id} value={guest.id}>
-                      {guest.lumaUser.name ?? 'Anonymous'}
-                    </option>
+                    <SelectItem key={guest.id} value={guest.id}>
+                      <HStack>
+                        <Avatar className="mr-2">
+                          <AvatarImage
+                            src={guest.lumaUser.avatarUrl}
+                            loading="lazy"
+                          />
+                          <AvatarFallback>{guest.lumaUser.name}</AvatarFallback>
+                        </Avatar>
+
+                        <p>{guest.lumaUser.name ?? 'Anonymous'}</p>
+                      </HStack>
+                    </SelectItem>
                   ))}
-                </Select>
-                <FormErrorMessage>{hostId.error}</FormErrorMessage>
-              </FormControl>
+                </SelectContent>
+              </Select>
+              <div className="text-destructive">{hostId.error}</div>
+            </fieldset>
 
-              <FormControl isInvalid={!!zoomUrl.error}>
-                <FormLabel>
-                  Zoom URL{' '}
-                  <Text display="inline" fontSize="xs" color="gray.400">
-                    Optional
-                  </Text>
-                </FormLabel>
-                <Input
-                  name={zoomUrl.name}
-                  defaultValue={zoomUrl.defaultValue}
-                />
-                <FormErrorMessage>{zoomUrl.error}</FormErrorMessage>
-              </FormControl>
+            <fieldset>
+              <Label htmlFor={zoomUrl.id}>
+                Zoom URL{' '}
+                <span className="text-xs text-slate-400">Optional</span>
+              </Label>
+              <Input
+                {...conform.input(zoomUrl)}
+                name={zoomUrl.name}
+                defaultValue={zoomUrl.defaultValue}
+              />
+              <div className="text-destructive">{zoomUrl.error}</div>
+            </fieldset>
 
-              <FormControl isInvalid={!!state.error}>
-                <FormLabel>State</FormLabel>
-                <RadioGroup name={state.name} defaultValue={state.defaultValue}>
-                  <HStack spacing="4">
-                    <Radio value="In Preparation" defaultChecked>
-                      In Preparation
-                    </Radio>
-                    <Radio value="On Live">On Live</Radio>
-                    <Radio value="Finished">Finished</Radio>
+            <fieldset>
+              <Label htmlFor={state.id}>State</Label>
+              <RadioGroup name={state.name} defaultValue={state.defaultValue}>
+                <Stack>
+                  <HStack>
+                    <RadioGroupItem
+                      value="In Preparation"
+                      id="preparation"
+                      defaultChecked
+                    />
+                    <Label htmlFor="preparation">In Preparation</Label>
                   </HStack>
-                </RadioGroup>
-                <FormErrorMessage>{state.error}</FormErrorMessage>
-              </FormControl>
-            </Stack>
-          </Form>
-        </ModalBody>
 
-        <ModalFooter>
+                  <HStack>
+                    <RadioGroupItem value="On Live" id="online" />
+                    <Label htmlFor="online">On Live</Label>
+                  </HStack>
+
+                  <HStack>
+                    <RadioGroupItem value="Finished" id="finished" />
+                    <Label htmlFor="finished">Finished</Label>
+                  </HStack>
+                </Stack>
+              </RadioGroup>
+              <div className="text-destructive">{state.error}</div>
+            </fieldset>
+          </Stack>
+        </Form>
+
+        <DialogFooter>
           <HStack>
             <Button type="submit" form="track-edit-form">
               Submit
@@ -139,8 +161,8 @@ export default function TrackAddPage() {
               Cancel
             </Button>
           </HStack>
-        </ModalFooter>
-      </ModalContent>
-    </Modal>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   )
 }
