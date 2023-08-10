bd8dd4c844c0ec08d412739ac626baf0036fb6ef
coji <coji@techtalk.jp>
2023-08-10 11:17:32 +0900

    fix: container

--- app/components/AppHeader.tsx
+++ app/components/AppHeader.tsx
@@ -8,15 +8,14 @@ interface AppHeaderProps extends React.HTMLAttributes<HTMLDivElement> {
 }
 export const AppHeader = ({ title, to, ...rest }: AppHeaderProps) => {
   return (
-    <div
-      className="container flex items-center bg-background text-foreground "
-      {...rest}
-    >
-      <Heading className="py-2 text-center">
-        <Link to={to}>{title}</Link>
-      </Heading>
-      <Spacer />
-      <AppLoginPane />
-    </div>
+    <header className="bg-background text-foreground " {...rest}>
+      <div className="container flex items-center ">
+        <Heading className="py-2 text-center">
+          <Link to={to}>{title}</Link>
+        </Heading>
+        <Spacer />
+        <AppLoginPane />
+      </div>
+    </header>
   )
 }
--- app/routes/_app+/_layout.tsx
+++ app/routes/_app+/_layout.tsx
@@ -20,9 +20,9 @@ export default function AppLayout() {
   const breadcrumbs = useAppBreadcrumbs()
 
   return (
-    <div className="grid min-h-screen grid-rows-[auto_1fr_auto]">
+    <div className="grid min-h-screen grid-rows-[auto_1fr_auto] bg-slate-200">
       <AppHeader title="LLM Meetup Tokyo" to="/" />
-      <Stack className="container gap-0 bg-slate-200">
+      <Stack className="container gap-0">
         <AppBreadcrumbs items={breadcrumbs} />
         <Outlet />
       </Stack>
