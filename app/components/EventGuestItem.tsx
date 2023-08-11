19522bc812db7e8356accbab9a340e092f3a84fd
coji <coji@techtalk.jp>
2023-08-11 23:23:48 +0900

    fix: layout

--- app/components/EventGuestItem.tsx
+++ app/components/EventGuestItem.tsx
@@ -16,15 +16,15 @@ export const EventGuestItem = ({
   clusterIndex,
 }: EventGuestItemProps) => {
   return (
-    <Stack>
-      <HStack className="md:w-16rem w-auto gap-4">
+    <Stack className="flex-col md:flex-row md:items-start">
+      <HStack className="w-auto gap-4 md:w-[16rem]">
         <Avatar>
           <AvatarImage src={avatarUrl} loading="lazy" />
         </Avatar>
         <div>
           <p>{name}</p>
 
-          <p className="break-all text-xs">
+          <p className="break-all text-xs text-slate-500 hover:text-primary">
             <Linkify
               options={{
                 defaultProtocol: 'https',
@@ -37,14 +37,14 @@ export const EventGuestItem = ({
         </div>
       </HStack>
 
-      <div className="flex-1 break-all p-1 text-sm">
+      <div className="flex-1 break-all rounded px-2 py-1 text-sm ">
         <Linkify options={{ defaultProtocol: 'https', target: '_blank' }}>
           {demo}
         </Linkify>
       </div>
 
       {!!clusterIndex && (
-        <HStack className="text-sm">
+        <HStack className="text-xs text-slate-500">
           <p>クラスタ</p> <Badge variant="outline">{clusterIndex}</Badge>
         </HStack>
       )}
